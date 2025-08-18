143f3964bd0cc88f9b9e217b7a9840896bbac528
yangFenTuoZi <140885477+yangFenTuoZi@users.noreply.github.com>
2025-08-18 12:21:31 +0800

    ui: 重构“设置”

--- app/src/main/java/yangfentuozi/batteryrecorder/ui/preference/MaterialSwitchPreference.kt
+++ app/src/main/java/yangfentuozi/batteryrecorder/ui/preference/MaterialSwitchPreference.kt
@@ -2,9 +2,7 @@ package yangfentuozi.batteryrecorder.ui.preference
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.preference.PreferenceViewHolder
 import androidx.preference.SwitchPreferenceCompat
-import com.google.android.material.materialswitch.MaterialSwitch
 import yangfentuozi.batteryrecorder.R
 
 class MaterialSwitchPreference @JvmOverloads constructor(
@@ -16,26 +14,5 @@ class MaterialSwitchPreference @JvmOverloads constructor(
 
     init {
         widgetLayoutResource = R.layout.preference_switch_md3
-    }
-
-    override fun onBindViewHolder(holder: PreferenceViewHolder) {
-        super.onBindViewHolder(holder)
-
-        val switchView = holder.findViewById(android.R.id.switch_widget) as? MaterialSwitch
-        switchView?.apply {
-            isChecked = this@MaterialSwitchPreference.isChecked
-
-            setOnCheckedChangeListener { buttonView, newValue ->
-                if (callChangeListener(newValue)) {
-                    this@MaterialSwitchPreference.isChecked = newValue
-                } else {
-                    isChecked = this@MaterialSwitchPreference.isChecked
-                }
-            }
-        }
-
-        holder.itemView.setOnClickListener {
-            switchView?.performClick()
-        }
     }
 }
--- app/src/main/java/yangfentuozi/batteryrecorder/ui/settings/SettingsFragment.kt
+++ app/src/main/java/yangfentuozi/batteryrecorder/ui/settings/SettingsFragment.kt
@@ -2,33 +2,123 @@ package yangfentuozi.batteryrecorder.ui.settings
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.core.content.edit
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceGroup
 import androidx.preference.PreferenceGroupAdapter
 import androidx.preference.PreferenceScreen
-import androidx.preference.SeekBarPreference
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import yangfentuozi.batteryrecorder.R
+import yangfentuozi.batteryrecorder.databinding.DialogBatchSizeBinding
+import yangfentuozi.batteryrecorder.databinding.DialogCurrentUnitCalibrationBinding
+import yangfentuozi.batteryrecorder.databinding.DialogIntervalBinding
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    @SuppressLint("SetTextI18n")
     override fun onCreatePreferences(
         savedInstanceState: Bundle?,
         rootKey: String?
     ) {
         addPreferencesFromResource(R.xml.preferences)
 
-        findPreference<SeekBarPreference>("test")?.setOnPreferenceChangeListener { preference, newValue ->
-            val progress = newValue as Int
-            val step = 100  // 你的步进值
-            val min = 0
-            val newValue = ((progress - min + step / 2) / step) * step + min
-            if (newValue != progress) {
-                (preference as SeekBarPreference).value = newValue
+        findPreference<Preference>("current_unit_calibration")?.setOnPreferenceClickListener { preference ->
+            val binding = DialogCurrentUnitCalibrationBinding.inflate(layoutInflater, null, false)
+            val maxValue = 100000000
+            var value = preference.sharedPreferences?.getInt(preference.key, -1) ?: -1
+            binding.value.text = value.toString()
+
+            binding.subtract.apply {
+                setOnClickListener {
+                    if (value < 0) value *= 10
+                    else value /= 10
+                    if (value == 0) value = -1
+                    if (value < -maxValue) value = -maxValue
+                    binding.value.text = value.toString()
+                }
             }
+
+            binding.add.apply {
+                setOnClickListener {
+                    if (value > 0) value *= 10
+                    else value /= 10
+                    if (value == 0) value = 1
+                    if (value > maxValue) value = maxValue
+                    binding.value.text = value.toString()
+                }
+            }
+
+            fun saveData() {
+                preference.sharedPreferences?.edit {
+                    putInt(preference.key, value)
+                }
+            }
+
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.preference_current_unit_calibration)
+                .setView(binding.root)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    saveData()
+                }
+                .setNegativeButton(android.R.string.cancel) { _, _ -> }
+                .setNeutralButton(R.string.reset) { _, _ ->
+                    value = -1
+                    saveData()
+                }
+                .show()
             true
         }
 
+        findPreference<Preference>("interval")?.setOnPreferenceClickListener { preference ->
+            val binding = DialogIntervalBinding.inflate(layoutInflater, null, false)
+            binding.slider.value = (preference.sharedPreferences?.getInt(preference.key, 900) ?: 900) / 100f
+
+            fun saveData() {
+                preference.sharedPreferences?.edit {
+                    putInt(preference.key, (binding.slider.value * 100).toInt())
+                }
+            }
+
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.preference_interval)
+                .setView(binding.root)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    saveData()
+                }
+                .setNegativeButton(android.R.string.cancel) { _, _ -> }
+                .setNeutralButton(R.string.reset) { _, _ ->
+                    binding.slider.value = 0.9f
+                    saveData()
+                }
+                .show()
+            return@setOnPreferenceClickListener true
+        }
+
+        findPreference<Preference>("batch_size")?.setOnPreferenceClickListener { preference ->
+            val binding = DialogBatchSizeBinding.inflate(layoutInflater, null, false)
+            binding.editText.setText((preference.sharedPreferences?.getInt(preference.key, 20) ?: 20).toString())
+
+            fun saveData() {
+                preference.sharedPreferences?.edit {
+                    putInt(preference.key, binding.editText.text.toString().toInt())
+                }
+            }
+
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.preference_batch_size)
+                .setView(binding.root)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    saveData()
+                }
+                .setNegativeButton(android.R.string.cancel) { _, _ -> }
+                .setNeutralButton(R.string.reset) { _, _ ->
+                    binding.editText.setText("20")
+                    saveData()
+                }
+                .show()
+            return@setOnPreferenceClickListener true
+        }
     }
 
     private fun setAllPreferencesToAvoidHavingExtraSpace(preference: Preference) {
