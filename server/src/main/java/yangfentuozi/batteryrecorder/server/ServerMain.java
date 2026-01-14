131e73150fb02f2aee8f3ce02ea56ea19118c700
yangFenTuoZi <140885477+yangFenTuoZi@users.noreply.github.com>
2026-01-14 22:21:34 +0800

    server: 优化进程调度，锁定 CPU 核心 1

--- server/src/main/java/yangfentuozi/batteryrecorder/server/ServerMain.java
+++ server/src/main/java/yangfentuozi/batteryrecorder/server/ServerMain.java
@@ -3,9 +3,16 @@ package yangfentuozi.batteryrecorder.server;
 import android.ddm.DdmHandleAppName;
 import android.system.Os;
 
+import java.io.IOException;
+
 public class ServerMain {
     public static void main(String[] args) {
         DdmHandleAppName.setAppName("battery_recorder", Os.getuid());
+        try {
+            Runtime.getRuntime().exec(new String[]{"taskset", "-ap", "1", String.valueOf(Os.getpid())});
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         new Server();
     }
 }
