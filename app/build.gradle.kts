4b7259d42027819b0f1208092961ece64350c158
yangFenTuoZi <140885477+yangFenTuoZi@users.noreply.github.com>
2026-02-15 23:26:22 +0800

    feat: 自动 ci 构建

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -19,6 +22,26 @@ fun List<String>.execute(workingDir: File): String {
         "unknown" // fallback value
     }
 }
+
+val ksFile = rootProject.file("signing.properties")
+val props = Properties()
+if (ksFile.canRead()) {
+    props.load(FileInputStream(ksFile))
+    android.signingConfigs.create("sign").apply {
+        storeFile = file(props["KEYSTORE_FILE"] as String)
+        storePassword = props["KEYSTORE_PASSWORD"] as String
+        keyAlias = props["KEYSTORE_ALIAS"] as String
+        keyPassword = props["KEYSTORE_ALIAS_PASSWORD"] as String
+    }
+} else {
+    android.signingConfigs.create("sign").apply {
+        storeFile = android.signingConfigs.getByName("debug").storeFile
+        storePassword = android.signingConfigs.getByName("debug").storePassword
+        keyAlias = android.signingConfigs.getByName("debug").keyAlias
+        keyPassword = android.signingConfigs.getByName("debug").keyPassword
+    }
+}
+
 android {
     namespace = "yangfentuozi.batteryrecorder"
     compileSdk = 36
@@ -36,6 +59,10 @@ android {
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles("proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("sign")
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("sign")
         }
     }
 
@@ -59,6 +86,28 @@ android {
     packaging {
         jniLibs {
             useLegacyPackaging = true
+        }
+    }
+    applicationVariants.all {
+        outputs.all {
+            (this as com.android.build.gradle.internal.api.BaseVariantOutputImpl).outputFileName = "batteryrecorder-v${versionName}-${name}.apk"
+            assembleProvider.get().doLast {
+                val outDir = File(rootDir, "out")
+                val mappingDir = File(outDir, "mapping").absolutePath
+                val apkDir = File(outDir, "apk").absolutePath
+
+                if (buildType.isMinifyEnabled) {
+                    copy {
+                        from(mappingFileProvider.get())
+                        into(mappingDir)
+                        rename { _ -> "mapping-${versionName}.txt" }
+                    }
+                    copy {
+                        from(outputFile)
+                        into(apkDir)
+                    }
+                }
+            }
         }
     }
 }
