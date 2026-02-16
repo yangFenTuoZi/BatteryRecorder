ac4218b9f49dbdc485efa725b1f461b9a2de82b6
yangFenTuoZi <140885477+yangFenTuoZi@users.noreply.github.com>
2026-02-17 01:04:48 +0800

    fix(server): 去掉自行 taskset，解决在某些 ColorOS 设备上息屏后服务被异常暂停的问题

--- server/src/main/java/yangfentuozi/batteryrecorder/server/Main.java
+++ server/src/main/java/yangfentuozi/batteryrecorder/server/Main.java
@@ -1,24 +1,22 @@
 package yangfentuozi.batteryrecorder.server;
 
 import android.ddm.DdmHandleAppName;
-import android.system.Os;
-import android.util.Log;
 
 import androidx.annotation.Keep;
-
-import java.io.IOException;
 
 @Keep
 public class Main {
     @Keep
     public static void main(String[] args) {
         DdmHandleAppName.setAppName("battery_recorder", 0);
+        /* ColorOS 调度使得某些 Soc 的 1 核在息屏后被禁用，会导致某些机型息屏后无法正常记录，故不自行 taskset，全权交由系统调度
         try {
             Runtime.getRuntime().exec(new String[]{"taskset", "-ap", "1", String.valueOf(Os.getpid())});
         } catch (IOException e) {
             Log.e("Main", "Failed to set task affinity", e);
             throw new RuntimeException(e);
         }
+        */
         new Server();
     }
 }
